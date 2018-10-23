89b2caef8df470f00d4d461ebd8d1650797e20aa
Patrik Fašang <patrik@digitalwerk.agency>
2018-10-24 00:43:56 +0200

    [TASK] flag-item component

--- src/App.vue
+++ src/App.vue
@@ -12,30 +12,40 @@
       </div>
       <div class="row">
         <div class="col-10">
-      <div class="row">
-        <div class="col-6 col-sm-4 col-md-2" v-for="n in houseCount">
-          <circle-item :selectedColor="color"></circle-item>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2">
-          <div @click="addHouse" class="circle-button">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="30" height="30" class="house-button-icon">
-                <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24 13 L 24 24 L 13 24 L 13 26 L 24 26 L 24 37 L 26 37 L 26 26 L 37 26 L 37 24 L 26 24 L 26 13 L 24 13 z"/>
-              </svg>
+          <div class="row">
+            <div class="col-6 col-sm-4 col-md-2" v-for="n in houseCount">
+              <flag-item :selectedColor="color"></flag-item>
+            </div>
+            <div class="col-6 col-sm-4 col-md-2">
+              <div @click="addHouse" class="flag-button button">
+
+                <div class="flag-button-item first"></div>
+                <div class="flag-button-item second"></div>
+                <div class="flag-button-item third"></div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="30" height="30" class="house-button-icon">
+                  <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24 13 L 24 24 L 13 24 L 13 26 L 24 26 L 24 37 L 26 37 L 26 26 L 37 26 L 37 24 L 26 24 L 26 13 L 24 13 z"/>
+                </svg>
+              </div>
+              <!--<div @click="addHouse" class="circle-button button">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="30" height="30" class="house-button-icon">
+                    <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24 13 L 24 24 L 13 24 L 13 26 L 24 26 L 24 37 L 26 37 L 26 26 L 37 26 L 37 24 L 26 24 L 26 13 L 24 13 z"/>
+                  </svg>
+              </div>-->
+            </div>
           </div>
         </div>
-      </div>
-    </div>
         <div class="col-1">
           <swatches v-model="color" :colors="colors" inline></swatches>
         </div>
+      </div>
     </div>
-    </div>
-    </div>
+  </div>
 </template>
 
 <script>
     import House from "./components/House";
     import CircleItem from "./components/CircleItem";
+    import FlagItem from "./components/FlagItem";
     import Swatches from "vue-swatches"
 
     export default {
@@ -49,23 +59,24 @@
             }
         },
         components: {
+            FlagItem,
             CircleItem,
             House,
             Swatches
         },
         methods: {
-          addHouse() {
-              this.houseCount++;
-              this.input = this.houseCount;
-          },
-          checkAnswer() {
-              if (Math.pow(this.colors.length, 2) == this.input) {
-                  alert("ok");
-              }
-              else {
-                  alert("wrong")
-              }
-          }
+            addHouse() {
+                this.houseCount++;
+                this.input = this.houseCount;
+            },
+            checkAnswer() {
+                if (Math.pow(this.colors.length, 2) == this.input) {
+                    alert("ok");
+                }
+                else {
+                    alert("wrong")
+                }
+            }
         }
     }
 </script>
@@ -80,8 +91,12 @@
     margin-top: 60px;
   }
 
-  .house-button {
+  .button {
+    cursor: pointer;
     position: relative;
+  }
+
+  .house-button {
     &:hover {
       .house-button-roof, .house-button-square {
         background: #777;
@@ -119,15 +134,15 @@
       z-index: 0;
     }
   }
+
   .circle-button {
     background: #AAA;
     border-radius: 50%;
     border: 2px solid black;
     height: 100px;
-    position: relative;
     width: 100px;
     &:hover {
-        background: #777;
+      background: #777;
     }
     &-icon {
       position: absolute;
@@ -138,4 +153,41 @@
       transform: translate(-50%, -50%);
     }
   }
+
+  .flag-button {
+    height: 99px;
+    &:hover {
+      .flag-button-item {
+        background: #777;
+      }
+    }
+    &-icon {
+      position: absolute;
+      background-color: #FFF;
+      border-radius: 50%;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    &-item {
+      width: 100%;
+      height: 33%;
+      background-color: #AAA;
+      border: 1px solid black;
+      position: absolute;
+      &.first {
+        top: 0;
+        border-bottom: 0;
+      }
+      &.second {
+        top: 33%;
+        border-bottom: 0;
+      }
+      &.third {
+        top: 66%;
+      }
+    }
+  }
+
 </style>
---
+++ src/components/FlagItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flag">
+    <div @click="changeFirstColor" class="flag-item first" :style="backgroundFirst"></div>
+    <div @click="changeSecondColor" class="flag-item second" :style="backgroundSecond"></div>
+    <div @click="changeThirdColor" class="flag-item third" :style="backgroundThird"></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "FlagItem",
+        props: ['selectedColor'],
+        data() {
+            return {
+                changed: true,
+                backgroundFirst: {
+                    backgroundColor: "white"
+                },
+                backgroundSecond: {
+                    backgroundColor: "white"
+                },
+                backgroundThird: {
+                    backgroundColor: "white"
+                }
+            }
+        },
+        methods: {
+            changeFirstColor() {
+                this.backgroundFirst.backgroundColor = this.selectedColor
+            },
+            changeSecondColor() {
+                this.backgroundSecond.backgroundColor = this.selectedColor
+            },
+            changeThirdColor() {
+                this.backgroundThird.backgroundColor = this.selectedColor
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+.flag {
+  position: relative;
+  height: 99px;
+
+  &-item {
+    width: 100%;
+    height: 33%;
+    border: 1px solid black;
+    position: absolute;
+    &.first {
+      top: 0;
+      border-bottom: 0;
+    }
+    &.second {
+      top: 33%;
+      border-bottom: 0;
+    }
+    &.third {
+      top: 66%;
+    }
+  }
+}
+</style>
